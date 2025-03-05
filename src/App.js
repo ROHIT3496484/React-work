47a91909566ae53c6cafeb76a1e19e847d8a31ab
Rohit Rana <84340668+ROHIT3496484@users.noreply.github.com>
2025-03-05 23:54:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import Autocompletesearch from "./Autocompletesearchbar/Autocompletesearch";
+import CountDownTimer from "./CountDownTimer/CountDownTimer";
+import Custommodel from "./Custom_Modal_overlay/Custommodel";
+import Emicalculate from "./EmiCalculate/Emicalculate";
+import Faqfront from "./Faq/Faqfront";
+import Faqmyselfcomp from "./Faqmyself.js/Faqmyselfcomp";
+import Multistepfirst from "./Multi-step form/Multistepfirst";
+import Shopping from "./Shopping list/Shopping";
 import Faqpage from "./FAQ/Faqpage";
 import "./styles.css";
 import React from "react";
@@ -6,7 +13,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <Faqpage/>
+      <Shopping/>
     </div>
   );
 }

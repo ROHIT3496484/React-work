a2ad93a54ad49ce7e1408bc00cfafc904767b301
Rohit Rana <84340668+ROHIT3496484@users.noreply.github.com>
2025-03-05 23:59:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Faqfront from "./Faq/Faqfront";
 import Faqmyselfcomp from "./Faqmyself.js/Faqmyselfcomp";
 import Multistepfirst from "./Multi-step form/Multistepfirst";
 import Shopping from "./Shopping list/Shopping";
-import Faqpage from "./FAQ/Faqpage";
+
+
 import "./styles.css";
 import React from "react";
 

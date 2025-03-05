5bab2b2bc275670430e01c99a8c1e79b7c0f9285
ROHIT3496484 <rohitrana349@gmail.com>
2025-03-05 23:28:51 +0530

    fun

--- src/Shopping list/Shopping.js
+++ src/Shopping list/Shopping.js
@@ -4,6 +4,7 @@ import './shopping.css'
 const Shopping = () => {
     const [food, setFood] = useState("");
     const [fetchfood, setFetchfood] = useState([]);
+    const [listed, setListed] = useState([]);
 
     const fetchdata =async()=>{
         const Response = await fetch(`https://dummyjson.com/recipes/search?q=${food}`)
@@ -11,13 +12,21 @@ const Shopping = () => {
         setFetchfood(data?.recipes || []);;
         console.log(data);
     }
-    useEffect((e)=>{
+    useEffect(()=>{
         if(food.length >=2){
-            fetchdata(food);
+            fetchdata();
         }
         
     },[food])
 
+    const addval=(items)=>{
+        setListed((prev)=>([...prev, items]));
+    }
+    const remval=(item)=>{
+        setListed((oldvalue) =>
+            oldvalue.filter(val => val !== item) 
+        );
+    }
   return (
     <div className='main'>
         <h1>Shopping-List</h1>
@@ -25,12 +34,24 @@ const Shopping = () => {
       <div>
         <input className='input' value= {food} type="text" onChange={(e)=>setFood(e.target.value)}></input>
       </div>
-      <div>
+      <div className="outshop">
         {
             fetchfood.map((mp) =>{
-                return <span key={mp.id}>
+                return <li className="product"key={mp.id}>
+                    <button onClick={()=>addval(mp.name)}>Y</button>
                     {mp.name}
-                </span>
+                </li>
+            })
+        }
+      </div>
+      <div>SelectedList</div>
+      <div className="outshop">
+        {
+            listed.map((mp) =>{
+                return <li className="product" > <input type='checkbox'/>
+                    {mp}
+                    <button onClick={()=>remval(mp)}>N</button>
+                </li>
             })
         }
       </div>

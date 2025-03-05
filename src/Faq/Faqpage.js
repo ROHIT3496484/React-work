54200aea489bf2a56cf887430326cfde1c4595ad
ROHIT3496484 <rohitrana349@gmail.com>
2025-03-06 00:08:10 +0530

    mer

--- src/Faq/Faqpage.js
+++ src/Faq/Faqpage.js
@@ -1,6 +1,29 @@
 import React from "react";
+import "./Faq.css";
 
-const Faqpage=()=>{
-
-}
+const Faqpage = () => {
+  const faqs = [
+    {
+      question: "How many bones does a cat have?",
+      answer: "A cat has 230 bones - 6 more than a human",
+    },
+    {
+      question: "How much do cats sleep?",
+      answer: "The average cat sleeps 12-16 hours per day",
+    },
+    {
+      question: "How long do cats live",
+      answer:
+        "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+    },
+  ];
+  return (
+    <div className="Faq-box">
+      <h1>Frequently Asked Question</h1>
+      {faqs.map((faq, index) => {
+        return <FaqComm faq={faq} index={index} />;
+      })}
+    </div>
+  );
+};
 export default Faqpage;
